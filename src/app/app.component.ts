2020f556e4742f61002573567e7fd7fa3040a97b
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-01-25 18:01:15 +0600

    complete app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Marker } from './marker';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gmap';
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = 23.771783;
+  lng: number = 90.363067;
+  lattitude: string;
+  longitude: string;
+  name: string;
+  markers: Marker[] = [
+    {
+      name: 'Dhaka',
+      lat: 23.771783,
+      lng: 90.363067,
+      draggable: true
+    },
+    {
+      name: 'Laskshmipur, bagbari',
+      lat: 22.944649,
+      lng: 90.835948,
+      draggable: true
+    },
+  ];
+
+  addMarker() {
+    let newMarker = {
+      name: this.name,
+      lat: parseFloat(this.lattitude),
+      lng: parseFloat(this.longitude),
+      draggable: true
+    }
+
+    this.markers.push(newMarker);
+  }
+
+  mapClicked($event: MouseEvent){
+    let newMarker = {
+      name: "untitled",
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: true
+    }
+
+    this.markers.push(newMarker);
+  }
+  markerClicked(marker:Marker, index:number) {
+    console.log(marker);
+  }
+  markerDragEnd(marker: Marker, $event: MouseEvent) {
+    let m = this.markers.find(x => x.name === marker.name);
+    let index = this.markers.indexOf(m);
+    console.log(index);
+    let updateMarker = {
+      name: marker.name,
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false
+    }
+
+    this.markers[index] = updateMarker;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -9,13 +10,14 @@ import { MAP_KEY } from './../config';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
       apiKey: MAP_KEY
-    })
+    }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
